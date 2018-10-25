20f28f3d9a3e117f143ce906937791211eec1d52
Iggy <igoririanto@rocketmail.com>
2018-10-25 18:11:06 -0500

    create passin session controller test

---
+++ test/phlox_web/controllers/session_controller_test.exs
@@ -0,0 +1,38 @@
+defmodule PhloxWeb.SessionControllerTest do
+  use PhloxWeb.ConnCase
+
+  alias Phlox.Accounts.User
+  alias Phlox.Repo
+
+  setup do
+    User.changeset(%User{}, %{username: "test", password: "test", password_confirmation: "test", email: "test@test.com"})
+    |> Repo.insert
+    {:ok, conn: build_conn()}
+  end
+
+  test "shows the login form", %{conn: conn} do
+    conn = get conn, session_path(conn, :new)
+    assert html_response(conn, 200) =~ "Login"
+  end
+
+  test "creates a new user session for a valid user", %{conn: conn} do
+    conn = post conn, session_path(conn, :create), user: %{username: "test", password: "test"}
+    assert get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Sign in successful"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+  test "does not create session with a bad login" do
+    conn = post conn, session_path(conn, :create), user: %{username: "test", password: "wrong"}
+    refute get_session(conn, :current_user)
+    assert get_flash(conn, :error) == "Invalid username/ password combination"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+  test "does not create a session if user does not exist", %{conn: conn} do
+    conn = post conn, session_path(conn, :create), user: %{username: "foo", password: "wrong"}
+    assert get_flash(conn, :error) == "Invalid username/ password combination"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+end
