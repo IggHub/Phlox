5bc3374dce7e590d7baead696c6cf04d96887a56
Iggy <igoririanto@rocketmail.com>
2018-10-26 13:20:40 -0500

    add layout view test and test logging in/out process

--- test/phlox_web/views/layout_view_test.exs
+++ test/phlox_web/views/layout_view_test.exs
@@ -1,3 +1,32 @@
 defmodule PhloxWeb.LayoutViewTest do
   use PhloxWeb.ConnCase, async: true
+
+  alias PhloxWeb.LayoutView
+  alias Phlox.Accounts.User
+  alias Phlox.Repo
+
+  setup do
+    User.changeset(%User{}, %{username: "test", password: "test", password_confirmation: "test", email: "test@test.com"})
+    |> Repo.insert
+    {:ok, conn: build_conn()}
+  end
+
+  test "current user returns the user in the session", %{conn: conn} do
+    conn = post conn, session_path(conn, :create), user: %{username: "test", password: "test"}
+    assert LayoutView.current_user(conn)
+  end
+
+  test "current user returns nothing if there is no user in the session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute LayoutView.current_user(conn)
+  end
+
+  test "deletes user session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Signed out successfully!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
 end
