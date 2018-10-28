0adda1ea6f55c47a437d1afca371dee736031bbd
Iggy <igoririanto@rocketmail.com>
2018-10-27 21:23:34 -0500

    edit user_post_path

--- lib/phlox_web/controllers/post_controller.ex
+++ lib/phlox_web/controllers/post_controller.ex
@@ -3,6 +3,8 @@ defmodule PhloxWeb.PostController do
   alias Phlox.Content
   alias Phlox.Content.Post
 
+  plug :assign_user
+
   def index(conn, _params) do
     posts = Content.list_posts()
     render(conn, "index.html", posts: posts)
@@ -14,11 +16,12 @@ defmodule PhloxWeb.PostController do
   end
 
   def create(conn, %{"post" => post_params}) do
+    changeset = Post.changeset(%Post{}, post_params)
     case Content.create_post(post_params) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post created successfully.")
-        |> redirect(to: post_path(conn, :show, post))
+        |> redirect(to: user_post_path(conn, :index, conn.assigns[:user]))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
@@ -36,24 +39,34 @@ defmodule PhloxWeb.PostController do
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Content.get_post!(id)
-
-    case Content.update_post(post, post_params) do
+    post = Repo.get!(Post, id)
+    changeset = Post.changeset(post, post_params)
+    case Repo.update(changeset) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post updated successfully.")
-        |> redirect(to: post_path(conn, :show, post))
-      {:error, %Ecto.Changeset{} = changeset} ->
+        |> redirect(to: user_post_path(conn, :show, conn.assigns[:user], post))
+      {:error, changeset} ->
         render(conn, "edit.html", post: post, changeset: changeset)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    post = Content.get_post!(id)
-    {:ok, _post} = Content.delete_post(post)
+    post = Content.get_post!(Post, id)
+    Repo.delete!(post)
 
     conn
     |> put_flash(:info, "Post deleted successfully.")
-    |> redirect(to: post_path(conn, :index))
+    |> redirect(to: user_post_path(conn, :index, conn.assigns[:user]))
+  end
+
+  defp assign_user(conn, _opts) do
+    case conn.params do
+      %{"user_id" => user_id} ->
+        user = Repo.get(Pxblog.User, user_id)
+        assign(conn, :user, user)
+      _ ->
+        conn
+    end
   end
 end
