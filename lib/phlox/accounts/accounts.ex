5b78afbce70897fdb2831e1bf76cc801d8a722a8
Iggy <igoririanto@rocketmail.com>
2018-10-31 12:53:17 -0500

    create role context

--- lib/phlox/accounts/accounts.ex
+++ lib/phlox/accounts/accounts.ex
@@ -1,12 +1,9 @@
 defmodule Phlox.Accounts do
-  @moduledoc """
-  The Accounts context.
-  """
-
   import Ecto.Query, warn: false
   alias Phlox.Repo
 
   alias Phlox.Accounts.User
+  alias Phlox.Accounts.Role
 
   def list_users do
     Repo.all(User)
@@ -32,5 +29,31 @@ defmodule Phlox.Accounts do
 
   def change_user(%User{} = user) do
     User.changeset(user, %{})
+  end
+
+  def list_roles do
+    Repo.all(Role)
+  end
+
+  def get_role!(id), do: Repo.get!(Role, id)
+
+  def create_role(attrs \\ %{}) do
+    %Role{}
+    |> Role.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_role(%Role{} = role, attrs) do
+    role
+    |> Role.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_role(%Role{} = role) do
+    Repo.delete(role)
+  end
+
+  def change_role(%Role{} = role) do
+    Role.changeset(role, %{})
   end
 end
