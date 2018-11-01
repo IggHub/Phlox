9ab496d41091ecf35f34b8b6d296796c8d01c97d
Iggy <igoririanto@rocketmail.com>
2018-11-01 18:23:22 -0500

    passing layout view test

--- test/phlox_web/views/layout_view_test.exs
+++ test/phlox_web/views/layout_view_test.exs
@@ -1,29 +1,27 @@
 defmodule PhloxWeb.LayoutViewTest do
   use PhloxWeb.ConnCase, async: true
-
+  alias Phlox.TestHelper
   alias PhloxWeb.LayoutView
   alias Phlox.Accounts.User
   alias Phlox.Repo
 
   setup do
-    User.changeset(%User{}, %{username: "test", password: "test", password_confirmation: "test", email: "test@test.com"})
-    |> Repo.insert
-    {:ok, conn: build_conn()}
+    {:ok, role} = TestHelper.create_role(%{name: "Some Role", admin: false})
+    {:ok, user} = TestHelper.create_user(role, %{username: "Test User", email: "test@email.com", password: "password", password_confirmation: "password"})
+    {:ok, conn: build_conn(), user: user}
   end
 
-  test "current user returns the user in the session", %{conn: conn} do
-    conn = post conn, session_path(conn, :create), user: %{username: "test", password: "test"}
+  test "current user returns the user in the session", %{conn: conn, user: user} do
+    conn = post conn, session_path(conn, :create), user: %{username: user.username, password: user.password}
     assert LayoutView.current_user(conn)
   end
 
-  test "current user returns nothing if there is no user in the session", %{conn: conn} do
-    user = Repo.get_by(User, %{username: "test"})
+  test "current user returns nothing if there is no user in the session", %{conn: conn, user: user} do
     conn = delete conn, session_path(conn, :delete, user)
     refute LayoutView.current_user(conn)
   end
 
-  test "deletes user session", %{conn: conn} do
-    user = Repo.get_by(User, %{username: "test"})
+  test "deletes user session", %{conn: conn, user: user} do
     conn = delete conn, session_path(conn, :delete, user)
     refute get_session(conn, :current_user)
     assert get_flash(conn, :info) == "Signed out successfully!"
