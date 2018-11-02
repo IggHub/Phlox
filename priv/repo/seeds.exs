87b8f3b78b70ab95c23fda1ea6679e8653ad6e47
Iggy <igoririanto@rocketmail.com>
2018-11-02 12:54:30 -0500

    add admin seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Phlox.Repo
+alias Phlox.Accounts.{Role, User}
+
+role = %Role{}
+  |> Role.changeset(%{name: "Admin", admin: true})
+  |> Repo.insert!
+
+admin = %User{}
+  |> User.changeset(%{role_id: role.id, username: "admin", password: "test", password_confirmation: "test", email: "admin@email.com"})
+  |> Repo.insert!
