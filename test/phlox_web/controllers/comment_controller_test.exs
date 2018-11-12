28e85b31802dcad9db3a6428ee3a65d9a3668e15
Iggy <igoririanto@rocketmail.com>
2018-11-12 17:54:58 -0600

    add delete comment test

--- test/phlox_web/controllers/comment_controller_test.exs
+++ test/phlox_web/controllers/comment_controller_test.exs
@@ -3,6 +3,7 @@ defmodule PhloxWeb.CommentControllerTest do
   import Phlox.Factory
 
   alias Phlox.Repo
+  alias Phlox.Content.Comment
   use PhloxWeb.ConnCase
 
   @valid_attrs %{author: "Some Person", body: "This is a sample comment", approved: false}
@@ -11,8 +12,9 @@ defmodule PhloxWeb.CommentControllerTest do
   setup do
     user = insert(:user)
     post = insert(:post, user: user)
+    comment = insert(:comment)
 
-    {:ok, conn: build_conn(), user: user, post: post}
+    {:ok, conn: build_conn(), user: user, post: post, comment: comment}
   end
 
   test "creates resource and redirects when data is valid", %{conn: conn, post: post} do
@@ -24,5 +26,12 @@ defmodule PhloxWeb.CommentControllerTest do
   test "does not create resource and renders errors when data is invalid", %{conn: conn, post: post} do
     conn = post conn, post_comment_path(conn, :create, post), comment: @invalid_attrs
     assert html_response(conn, 200) =~ "Oops, something went wrong"
+  end
+
+  test "deletes the comment", %{conn: conn, comment: comment, post: post} do
+    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+
+    assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
+    refute Repo.get(Comment, comment.id)
   end
 end
