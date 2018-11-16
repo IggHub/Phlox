3ead1f8b2fb80fcd4cf3cdfad117d9e582b469af
Iggy <igoririanto@rocketmail.com>
2018-11-15 18:03:58 -0600

    passing comment helper test

--- test/phlox_web/channels/comment_helper_test.exs
+++ test/phlox_web/channels/comment_helper_test.exs
@@ -3,6 +3,7 @@ defmodule PhloxWeb.CommentHelperTest do
 
   alias Phlox.Content.Comment
   alias PhloxWeb.CommentHelper
+  alias Phlox.Repo
 
   import Phlox.Factory
 
@@ -13,5 +14,35 @@ defmodule PhloxWeb.CommentHelperTest do
     fake_socket = %{assigns: %{user: user.id}}
 
     {:ok, user: user, post: post, comment: comment, socket: fake_socket}
+  end
+
+  test "creates a comment for a post", %{post: post} do
+    {:ok, comment} = CommentHelper.create(%{
+          "postId" => post.id,
+          "author" => "Some Person",
+          "body" => "Some Post"
+                                          }, %{})
+    assert comment
+    assert Repo.get(Comment, comment.id)
+  end
+
+  test "approves a comment when an authorized user", %{post: post, comment: comment, socket: socket} do
+    {:ok, comment} = CommentHelper.approve(%{"postId" => post.id, "commentId" => comment.id}, socket)
+    assert comment.approved
+  end
+
+  test "does not approve a comment when not an authorized user", %{post: post, comment: comment} do
+    {:error, message} = CommentHelper.approve(%{"postId" => post.id, "commentId" => comment.id}, %{})
+    assert message == "User is not authorized"
+  end
+
+  test "deletes a comment when an authorized user", %{post: post, comment: comment, socket: socket} do
+    {:ok, comment} = CommentHelper.delete(%{"postId" => post.id, "commentId" => comment.id}, socket)
+    refute Repo.get(Comment, comment.id)
+  end
+
+  test "does not delete a comment when not an authorized user", %{post: post, comment: comment, socket: socket} do
+    {:error, message} = CommentHelper.delete(%{"postId" => post.id, "commentId" => comment.id}, %{})
+    assert message == "User is not authorized"
   end
 end
