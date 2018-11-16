867524f5ad1a6174d9b2697799049b25d5a7aa2e
Iggy <igoririanto@rocketmail.com>
2018-11-15 18:19:37 -0600

    add cover and WIP comment_channel_test.exs

--- test/phlox_web/channels/comment_channel_test.exs
+++ test/phlox_web/channels/comment_channel_test.exs
@@ -2,13 +2,18 @@ defmodule PhloxWeb.CommentChannelTest do
   use PhloxWeb.ChannelCase
 
   alias PhloxWeb.CommentChannel
+  alias Phlox.Factory
 
   setup do
-    {:ok, _, socket} =
-      socket("user_id", %{some: :assign})
-      |> subscribe_and_join(CommentChannel, "comment:lobby")
+    # why not user = insert(:user) and why alias Phlox.Factory instead of import Phlox.Factory?
+    user = Factory.create(:user)
+    post = Factory.create(:post, user: user)
+    comment = Factory.create(:comment, post: post, approved: false)
 
-    {:ok, socket: socket}
+    {:ok, _, socket} = socket("user_id", %{user: user.id})
+    |> subscribe_and_join(CommentChannel, "comments:#{post.id}")
+
+    {:ok, socket: socket, post: post, comment: comment}
   end
 
   test "ping replies with status ok", %{socket: socket} do
@@ -16,13 +21,15 @@ defmodule PhloxWeb.CommentChannelTest do
     assert_reply ref, :ok, %{"hello" => "there"}
   end
 
-  test "shout broadcasts to comment:lobby", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
-
   test "broadcasts are pushed to the client", %{socket: socket} do
     broadcast_from! socket, "broadcast", %{"some" => "data"}
     assert_push "broadcast", %{"some" => "data"}
+  end
+
+  test "CREATED_COMMENT broadcasts to comments:*", %{socket: socket, post: post} do
+    push socket, "CREATED_COMMENT", %{"body" => "Test Post", "author" => "Test Author", "postId" => post.id}
+    expected = %{"body" => "Test Post", "author" => "Test Author"}
+
+    assert broadcast "CREATED_COMMENT", expected
   end
 end
